c9f1ac6684a7f621b5f3ec775af8578b27abff2b
Rodrigo C Oliveira <rodrigocezaroliveira014@gmail.com>
2022-07-16 10:55:56 -0300

    main

--- index.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('#t1').textContent = 'JavaScript - Introdução';
-document.querySelector('#t2').textContent = 'Exemplificando algumas funcionalidades no JS';
-
-function bemVindo(){
-    const nome = document.querySelector('#nomeUsuario').value;
-    alert('Olá ${nome}! Seja bem-vindo!');
-    document.getElementById('formulariologin').style.display = 'none';
-
-}
